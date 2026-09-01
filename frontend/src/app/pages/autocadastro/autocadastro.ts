8045f3d74ddaac677aa47d1a2ca1b33e3a21ae90
ArthurToso <arthurtoso2003@gmail.com>
2026-08-31 22:10:37 -0300

    RF001: Estilizacao Bootstrap

--- frontend/src/app/pages/autocadastro/autocadastro.ts
+++ frontend/src/app/pages/autocadastro/autocadastro.ts
@@ -14,6 +14,11 @@ export class Autocadastro {
   private http = inject(HttpClient)
   cepNaoEncontrado: boolean = false
   erroRequisicao: boolean = false
+  ufs = ['AC', 'AL', 'AP', 'AM', 'BA',
+    'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 
+    'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI',
+    'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP',
+    'SE', 'TO']
   
   cadastroForm = new FormGroup({
     cpf: new FormControl('',
